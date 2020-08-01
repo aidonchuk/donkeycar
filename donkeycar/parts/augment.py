ef698e3e4d598b4cbf5d6443bbb2316284dcbf80
alex.dd84 <thibujixo>
2020-08-01 19:51:26 +0300

    albu

--- donkeycar/parts/augment.py
+++ donkeycar/parts/augment.py
@@ -3,12 +3,14 @@
     Author : Tawn Kramer
     Date : July 2017
 '''
+import glob
+import math
 import random
+
+import numpy as np
 from PIL import Image
 from PIL import ImageEnhance
-import glob
-import numpy as np
-import math
+from albumentations import Compose, OneOf, IAAAdditiveGaussianNoise, GaussNoise, ISONoise, Blur, MotionBlur, MedianBlur
 
 '''
     find_coeffs and persp_transform borrowed from:
@@ -20,8 +22,8 @@ ONE_BY_255 = 1.0 / 255.0
 def find_coeffs(pa, pb):
     matrix = []
     for p1, p2 in zip(pa, pb):
-        matrix.append([p1[0], p1[1], 1, 0, 0, 0, -p2[0]*p1[0], -p2[0]*p1[1]])
-        matrix.append([0, 0, 0, p1[0], p1[1], 1, -p2[1]*p1[0], -p2[1]*p1[1]])
+        matrix.append([p1[0], p1[1], 1, 0, 0, 0, -p2[0] * p1[0], -p2[0] * p1[1]])
+        matrix.append([0, 0, 0, p1[0], p1[1], 1, -p2[1] * p1[0], -p2[1] * p1[1]])
 
     A = np.matrix(matrix, dtype=np.float)
     B = np.array(pb).reshape(8)
@@ -63,6 +65,33 @@ def augment_image(np_img, shadow_images=None, do_warp_persp=False):
     return img_out
 
 
+def albu_transform(p=1):
+    return Compose([
+        OneOf([
+            # CLAHE(),
+            # IAASharpen(),
+            # IAAEmboss(),
+            # RandomBrightnessContrast(),
+            # RGBShift(),
+            # ImageCompression(),
+            # RandomGamma(),
+            # ChannelShuffle(),
+            # InvertImg(),
+            # HueSaturationValue()
+        ], p=0.3),
+        OneOf([
+            IAAAdditiveGaussianNoise(),
+            GaussNoise(),
+            ISONoise()
+        ], p=0.2),
+        OneOf([
+            Blur(),
+            MotionBlur(),
+            MedianBlur(),
+        ], p=0.2)
+    ], p=p)
+
+
 def augment_pil_image(img, shadow_images=None, do_warp_persp=False):
     """
     :param img: PIL image
@@ -74,6 +103,14 @@ def augment_pil_image(img, shadow_images=None, do_warp_persp=False):
     :return: PIL image
         augmented image
     """
+    use_albu = True
+    transform = albu_transform(p=1)
+    if use_albu: # TODO pil to numpy
+        data = {"image": img}
+        augmented = transform(**data)
+        img = augmented["image"]
+        # TODO numpy to pil
+
     # change the coloration, sharpness, and composite a shadow
     factor = random.uniform(0.5, 2.0)
     img = ImageEnhance.Brightness(img).enhance(factor)
@@ -113,5 +150,3 @@ def load_shadow_images(path_mask):
         mask = Image.merge("L", (a,))
         shadow_images.append((top, mask))
     return shadow_images
-
-
